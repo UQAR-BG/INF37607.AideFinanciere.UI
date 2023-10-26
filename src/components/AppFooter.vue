9b614368fdf6505e7d6231e32dfdf393a51092c4
Bastien Goulet <bastien.goulet@uqar.ca>
2023-10-26 16:20:24 -0400

    Added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,24 @@
 <template>
-    <footer>
-
+    <footer class="footer">
+        <ul class="footer-links">
+            <li class="small-label footer-link">
+                <a href="https://www.quebec.ca/gouvernement/ministere/education/acces-information">Accès à l'information</a>
+            </li><span class="vertical-line">|</span>
+            <li class="small-label footer-link">
+                <a href="https://www.education.gouv.qc.ca/accessibilite">Accessibilité</a>
+            </li><span class="vertical-line">|</span>
+            <li class="small-label footer-link">
+                <a href="https://www.quebec.ca/gouvernement/ministere/education/presence-du-ministere-sur-les-medias-sociaux/netiquette">Nétiquette</a>
+            </li><span class="vertical-line">|</span>
+            <li class="small-label footer-link">
+                <a href="https://www.education.gouv.qc.ca/plan-du-site">Plan du site</a>
+            </li><span class="vertical-line">|</span>
+            <li class="small-label footer-link">
+                <a href="https://www.education.gouv.qc.ca/politique-de-confidentialite">Politique de confidentialité</a>
+            </li>
+        </ul>
+        <img src="@/assets/images/quebecDrapeau_couleur.svg" alt="Logo" class="logo">
+        <a href="https://www.quebec.ca/droit-auteur" class="copyright small-label">&copy; Gouvernement du Québec, 2023</a>
     </footer>
 </template>
 
